4d5847f9f74ca75b2849e74bf6b641689707c0ea
hijeong_ <gmlwjdchl82@naver.com>
2022-12-07 22:45:45 +0900

    readme

---
+++ src/Db.js
@@ -0,0 +1,26 @@
+export const menuDb = [
+  {
+    id: 0,
+    name: "나이키 에어포스",
+    price: "129,000",
+    url: "https://photo.akmall.com/image2/goods/95/99/81/67/95998167_M_1500.jpg",
+  },
+  {
+    id: 1,
+    name: "나이키 범고래",
+    price: "153,000",
+    url: "https://image-cdn.hypb.st/https%3A%2F%2Fkr.hypebeast.com%2Ffiles%2F2022%2F03%2FNike-dunk-low-black-official-release-info-tw.jpg?w=960&cbr=1&q=90&fit=max",
+  },
+  {
+    id: 2,
+    name: "나이키 킬샷",
+    price: "109,000",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQix3nZLaRtA8rImz0Sd5QxHvMqV1poZM2RKA&usqp=CAU",
+  },
+  {
+    id: 3,
+    name: "나이키 시티코트",
+    price: "99,000",
+    url: "https://ae01.alicdn.com/kf/H9041677fea5d41bf94561e78f76912a6i/M2K-TEKNO-AV4789-100.jpg_Q90.jpg_.webp",
+  },
+];
